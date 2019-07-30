46c6eaac884f50e9ac76a8f7ccfc67a83b7e1814
Shashwat Bhattacharya <shashwat.mnit@gmail.com>
2019-07-30 17:59:58 +0530

    Temporarily changed the top wall boundary condition in thermal3.cc to Vx = 10

--- src/solvers/thermal/thermal3.cc
+++ src/solvers/thermal/thermal3.cc
@@ -735,7 +735,7 @@ void thermal_d3::imposeUBCs() {
         // Vx LIES ON THE BOTTOM WALL AS THE WALL IS ON STAGGERED POINT AND Vx IS STAGGERED ALONG Z
         V.Vx.F(V.Vx.fWalls(4)) = 0.0;
         // Vx LIES ON THE TOP WALL AS THE WALL IS ON STAGGERED POINT AND Vx IS STAGGERED ALONG Z
-        V.Vx.F(V.Vx.fWalls(5)) = 0.0;
+        V.Vx.F(V.Vx.fWalls(5)) = 10.0;
     }
 }
 
